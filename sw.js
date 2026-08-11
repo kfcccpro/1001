c99be8e31073aefdb329a5a2356fcff26f6d3e4b
KJB <sk01197375068@gmail.com>
2026-08-11 22:40:38 +0900

    Refresh PWA cache for daily learning v0.5

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='chunilmun-pfal-t1-v040';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./data/unit01.json'];
+const CACHE='chunilmun-pfal-t1-v050';
+const ASSETS=['./','./index.html','./styles.css','./learning.css','./app.js','./manifest.webmanifest','./data/unit01.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
