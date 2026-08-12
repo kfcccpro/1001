ce21bbc408326868efbc7723e289b7d4392f190d
KJB <sk01197375068@gmail.com>
2026-08-12 09:28:44 +0900

    Refresh PWA cache after v0.9.3 metadata fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='chunilmun-pfal-t1-v093';
+const CACHE='chunilmun-pfal-t1-v093a';
 const ASSETS=['./','./index.html','./styles.css','./learning.css','./growth-v06.css','./compare-v07.css','./pacing-v08.css','./readability-v081.css','./supervisor-v082.css','./cloud-v09.css','./cloud-diagnostic-v091.css','./guided-repair-v092.css','./compact-v093.css','./app.js','./growth-v06.js','./compare-v07.js','./pacing-v08.js','./supervisor-v082.js','./cloud-v09.js','./cloud-diagnostic-v091.js','./repair-guides-v092.js','./guided-repair-v092.js','./manifest.webmanifest','./data/unit01.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
